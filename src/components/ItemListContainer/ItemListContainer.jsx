063e4b3d8861674a0741f5d6c6f37c3a7d1fd28d
abonto <augustobontomasi@gmail.com>
2022-09-04 21:06:31 -0300

    Router agregado

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { useState,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
@@ -7,53 +6,16 @@ import ItemList from '../ItemList/ItemList'
 
 const ItemListContainer = () => {
 
-=======
-import React, {useEffect,useState} from 'react'
-import Products from '../../mock/products';
-import ItemList from '../ItemList/ItemList';
-import {useParams} from 'react-router-dom'
-import products from '../../mock/products';
-
-//Para agarrar un solo producto en el ItemDetailContainer
-//const producto1 = Products.find(producto => producto.id === 1)
->>>>>>> 2f2144b9521815de7713171ea00b6687e2e6b2d9
 
   const [items, setItems] = useState([]);
   
   //const {categoryName} = useParams();
 
-<<<<<<< HEAD
   // function filtroCategoria(item) {
   //     return item.category === categoryName
   // }
 
   const {id} = useParams()
-=======
-  const [list, setItems] = useState([]);
-
-  const {categoryName} = useParams();
-
-  useEffect(() => {
-    const getProducts = () =>
-        new Promise((res, rej) => {
-            const prodFiltrados = products.filter(
-                (prod) => prod.category === categoryName
-            );
-            setTimeout(() => {
-                res(categoryName ? prodFiltrados : products);
-            }, 500);
-        });
-
-    getProducts()
-        .then((data) => {
-            setItems(data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }, [categoryName]);
-
->>>>>>> 2f2144b9521815de7713171ea00b6687e2e6b2d9
 
   useEffect(()=>{
       const getProducts = new Promise((res, rej)=>{
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import CartWidget from '../CartWidget/CartWidget'
 import estilos from './navbar.module.css'
-import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
-<<<<<<< HEAD
       <>
         <Link to="/" className='titulo'><h1>TIENDA</h1></Link>
         <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center" >
@@ -37,30 +35,6 @@ const NavBar = () => {
         </nav>
       </>
   )
-=======
-      <nav className={estilos.nav}>
-          <Link to='/' className={estilos.titulo}>TIENDA SUPER</Link>
-          <ul className={estilos.lista}>
-            <Link to='/category/remeras' className={estilos.links}>REMERAS</Link>
-            <Link to='/category/gorras' className={estilos.links}>GORRA</Link>
-            <Link to='/category/calzado' className={estilos.links}>CALZADO</Link>
-          </ul>
-          <Link to='/cart'><CartWidget /></Link>
-      </nav>
-    )
-  }else{
-    return (
-      <nav className={estilos.nav}>
-          <h1 className={estilos.titulo}>ELECTRO</h1>
-          <ul className={estilos.lista}>
-            <li className={estilos.links}><a href="http:/google.com">TWITTER</a></li>
-            <li className={estilos.links}><a href="http:/google.com">FACEBOOK</a></li>
-            <li className={estilos.links}><a href="http:/google.com">CONTACTO</a></li>
-          </ul>
-      </nav>
-    )
-  }
->>>>>>> 2f2144b9521815de7713171ea00b6687e2e6b2d9
 }
 
 export default NavBar
